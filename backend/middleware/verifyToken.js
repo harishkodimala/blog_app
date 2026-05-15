99a6e7bc6cb7057dc97f3edfcfc27389c025cb30
Harish <24eg505k03@aanurag.edu.in>
2026-05-15 11:23:22 +0530

    fixed file casing1

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,50 +1,50 @@
 import jwt from "jsonwebtoken";
+import { config } from "dotenv";
 
-export const verifyToken = (role) => {
+config();
 
-  return (req, res, next) => {
+export const verifyToken = (...allowedRoles) => {
+  return async (req, res, next) => {
 
     try {
+      
 
-      // get token from cookies
-      const token = req.cookies.token;
+      // read token from cookies
+      let token = req.cookies.token;
+      console.log("token:", token);
+      console.log(req.cookies);
 
       if (!token) {
-        return res.status(401).json({
-          message: "Unauthorized. Token missing."
-        });
+        return res.status(401).json({ message: "Unauthorized request. Please login." });
       }
 
       // verify token
-      const decoded = jwt.verify(
-        token,
-        process.env.SECRET_KEY
-      );
+      let decodedToken = jwt.verify(token, process.env.JWT_SECRETKEY);
 
-      // attach user to request
-      req.user = decoded;
-console.log("Decoded user:", decoded);
-console.log("Required role:", role);
-      // IMPORTANT FIX
-      // only check role IF role is provided
-if (
-  role &&
-  decoded.role?.toUpperCase() !== role.toUpperCase()
-) {
-  return res.status(403).json({
-    message: "Forbidden. You dont have access."
-  });
-}
+      // check role authorization
+      if (!allowedRoles.includes(decodedToken.role)) {
+        return res.status(403).json({ message: "Forbidden. You dont have access." });
+      }
+      console.log("Decoded token", decodedToken.role)
+      decodedToken._id=decodedToken.userId;
+      console.log("Decoded token",decodedToken)
+      
+      req.user = decodedToken;
 
 
       next();
 
-    } catch (err) {
-
-      return res.status(401).json({
-        message: "Invalid or expired token."
-      });
-
+    } catch (error) {
+      if(error.name==="TokenExpiredError")
+      {
+        return res.status(401).json({message:"Session expired,Plz Login again"})
+      }
+      if(error.name==="JsonWebTokenError")
+      {
+        return res.status(401).json({message:"Invalid token,Plz login again"})
+      }
+      // next(error);
     }
+
   };
 };
